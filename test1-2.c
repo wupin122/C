3dfa23e948cf8abfcb61efa474e2cee9ba703b39
wu pin <2604047012@qq.com>
2018-03-27 00:08:21 +0800

    wrote a test1-1 file

---
+++ test1-2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	printf("天\n地\n人\n");
+	return 0;
+}
---
+++ test1-7.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    puts("天");
+    puts("地");
+    puts("人");
+    return 0;
+}
