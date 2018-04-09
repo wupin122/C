10b6c8cfb29d59feaa73b8e81bc200cef1d24aeb
wu pin <2604047012@qq.com>
2018-04-09 19:15:04 +0800

    test5-10

--- unit 5/test5-10.c
+++ unit 5/test5-10.c
@@ -4,21 +4,24 @@ int main(int argc, char const *argv[])
     int x[4][3],y[3][4],xy[4][4]={0};
     printf("请输入4行3列的矩阵。\n");
     for (int i = 0; i < 4; ++i)
-        for (int j = 0; j < 3; ++j){
+        for (int j = 0; j < 3; ++j)
+        {
             printf("[%d][%d]:", i+1,j+1);
             scanf("%d",&x[i][j]);
         }
     printf("请输入3行4列的矩阵。\n");
     for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 4; ++j){
+        for (int j = 0; j < 4; ++j)
+        {
             printf("[%d][%d]:", i+1,j+1);
-            scanf("%d",&x[i][j]);
+            scanf("%d",&y[i][j]);
         }
-    for (int i = 0; i < 4; ++i){
-        for (int j = 0; j < 3; ++j)
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
             for (int k = 0; k < 3; ++k)
             {
-                xy[i][j]+=(x[i][k]*y[k][j]);
+                xy[i][j]+=x[i][k]*y[k][j];
             }
     }
     printf("矩阵的乘积为：\n");
@@ -30,3 +33,4 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
+
